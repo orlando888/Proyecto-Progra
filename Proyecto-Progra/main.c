5a8ae71e0aa8263cec10161906a3826118e2ec69
ManuelPJ <55538534+ManuelPJ@users.noreply.github.com>
2019-11-27 10:55:14 +0100

    Add files via upload

---
+++ Proyecto-Progra/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	pritntf("1) Rellenar fichero de clientes \n");
+	pritntf("2) Componer el mensaje de texto \n");
+    pritntf("3) Formatear mensaje \n");
+	pritntf("4) Introducir clave\n");
+	pritntf("5) Codificar mensaje de un envío\n");
+	pritntf("6) Comprobar número del envío\n");
+	pritntf("7) Salir\n");
+	printf ("Opción :")
+	
+	switch()
+	{
+		case(1):
+		case(2):
+		case(3):
+		case(4):
+		case(5):
+		case(6):
+		case(7):
+	
+	}
+}
